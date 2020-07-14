d5c87152d48bb42170e754bb92b614b761bc047b
William Joel Clavell <wjclavell28@gmail.com>
2020-07-14 17:48:06 -0400

    image displayed in slider with description on top

--- portfolio/app.js
+++ portfolio/app.js
@@ -55,15 +55,11 @@ for (url in skills) {
 
 //*projects slider
 let mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
