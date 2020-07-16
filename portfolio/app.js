91a01c49e9c232691c6ecd95a23c5258a212a559
William Joel Clavell <wjclavell28@gmail.com>
2020-07-16 12:57:55 -0400

    projects slider json integration, hamburger menu onclick functions to close menu

--- portfolio/app.js
+++ portfolio/app.js
@@ -49,8 +49,8 @@ let skills = [
   "https://cdn3.iconfinder.com/data/icons/logos-brands-3/24/logo_brand_brands_logos_adobe_photoshop-512.png",
 ];
 //*loop to populate skills section with logos of my skills
-for (url in skills) {
-  $(".skill-container").append(`<img src="${skills[url]}">`);
+for (logo in skills) {
+  $(".skill-container").append(`<img src="${skills[logo]}">`);
 }
 
 //*projects slider
@@ -74,4 +74,79 @@ let mySwiper = new Swiper(".swiper-container", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+//*pull projects from googlesheets and populate projects slider with 'featured' projects
+const url =
+  "https://spreadsheets.google.com/feeds/list/18r08nO91byLTxZ8zVRghWGyLtIKrXzBa8P6sGM9T19M/od6/public/values?alt=json";
+
+fetch(url) //starts fetch process, to get the data
+  .then((response) => response.json()) //returns JSON data as a JS object
+  .then((data) => {
+    // console.log(data.feed.entry);
+    const projects = data.feed.entry.map((entry) => {
+      return {
+        title: entry.gsx$title.$t,
+        image: entry.gsx$image.$t,
+        description: entry.gsx$description.$t,
+        url: entry.gsx$url.$t,
+        category: entry.gsx$category.$t,
+        featured: entry.gsx$featured.$t,
+      };
+    });
+    app(projects); // calls the application with your new projects array as argument
+  });
+
+const app = (data) => {
+  console.log("app is running!");
+  console.log(data);
+
+  const createProjectElement = (project) => {
+    if (project.featured === "TRUE") {
+      const $projBox = $(`<div class="swiper-slide">
+    <div class="proj-head">
+      <a href="${project.url}"
+        ><h1>${project.title}</h1></a
+      >
+      <p>
+        ${project.description}
+      </p>
+    </div>
+    <a href="${project.url}">
+      <img
+        class="project-shown"
+        src="${project.image}"
+    /></a>
+  </div>`);
+
+      return $projBox;
+    }
+  };
+  //   $("body").append(createProjectElement(data[5]));
+
+  data.forEach((project) => {
+    const $projBox = createProjectElement(project);
+    $(".swiper-wrapper").append($projBox);
+  });
+};
+
+//*function to automatically close hamburger menu when you choose a link
+
+const $link1 = $("#link1");
+const $link2 = $("#link2");
+const $link3 = $("#link3");
+const $link4 = $("#link4");
+const $input = $("input");
+
+$link1.on("click", () => {
+  $input.prop("checked", false);
+});
+$link2.on("click", () => {
+  $input.prop("checked", false);
+});
+$link3.on("click", () => {
+  $input.prop("checked", false);
+});
+$link4.on("click", () => {
+  $input.prop("checked", false);
 });
